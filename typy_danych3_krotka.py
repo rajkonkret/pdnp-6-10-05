32044f44986092a316f8be782f0300abae3daebc
rajkonkret <rajkonkret660@gmail.com>
2024-05-06 15:49:30 +0200

    [1.11] update typy_danych3_krotka.py

--- typy_danych3_krotka.py
+++ typy_danych3_krotka.py
@@ -6,9 +6,9 @@ print(type(tupla))  # <class 'str'>
 
 tupla_2 = ("Radek")
 print(type(tupla_2))  # <class 'str'>
-tupla_names = ("Radek", "Tomek", "Asia", "Zbyszek", "Marek")
-print(type(tupla_names))  # <class 'tuple'>
-print(tupla_names)  # ('Radek', 'Tomek', 'Asia', 'Zbyszek', 'Marek')
+tupla_names_new = ("Radek", "Tomek", "Asia", "Zbyszek", "Marek")
+print(type(tupla_names_new))  # <class 'tuple'>
+print(tupla_names_new)  # ('Radek', 'Tomek', 'Asia', 'Zbyszek', 'Marek')
 tupla_names2 = "Radek", "Tomek", "Asia", "Zbyszek", "Marek"
 print(type(tupla_names2))  # <class 'tuple'>
 tupla_3 = ("Radek",)  # PEP8 zaleca tuple jednoelemntowa z nawaiasami
@@ -23,9 +23,9 @@ print(type(tupla_liczby))  # <class 'tuple'>
 
 # tupla_liczby[0] = 2  # TypeError: 'tuple' object does not support item assignment
 
-print(tupla_names.count("Tomek"))  # 1
-print(tupla_names.index("Tomek"))  # 1
-print(sorted(tupla_names))  # sortowanie tupli daje liste ['Asia', 'Marek', 'Radek', 'Tomek', 'Zbyszek']
+print(tupla_names_new.count("Tomek"))  # 1
+print(tupla_names_new.index("Tomek"))  # 1
+print(sorted(tupla_names_new))  # sortowanie tupli daje liste ['Asia', 'Marek', 'Radek', 'Tomek', 'Zbyszek']
 
 a, b = 1, 2
 print(a)
@@ -37,18 +37,18 @@ a, *b = 1, 2, 3  # * worek na pozostałe dane
 print(a)  # 1
 print(b)  # [2, 3]
 
-imie, *imie2, imie3 = tupla_names
+imie, *imie2, imie3 = tupla_names_new
 print(imie, imie2, imie3)  # Radek ['Tomek', 'Asia', 'Zbyszek'] Marek
 
-*imie, imie2, imie3 = tupla_names
+*imie, imie2, imie3 = tupla_names_new
 print(imie, imie2, imie3)
 # ['Radek', 'Tomek', 'Asia'] Zbyszek Marek
 
 
-imie, imie2, *imie3 = tupla_names
+imie, imie2, *imie3 = tupla_names_new
 print(imie, imie2, imie3)
 # Radek Tomek ['Asia', 'Zbyszek', 'Marek']
 
-lista = list(tupla_names)
+lista = list(tupla_names_new)
 print(lista)  # ['Radek', 'Tomek', 'Asia', 'Zbyszek', 'Marek']
 print(type(lista))  # <class 'list'>
