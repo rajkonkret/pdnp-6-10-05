9363e4f6d14194d292b984d1a94e4c69a53c750c
rajkonkret <rajkonkret660@gmail.com>
2024-05-08 10:26:32 +0200

    [3.4] add json_zad1.py

---
+++ json_zad1.py
@@ -0,0 +1,48 @@
+import json
+
+# json - {"name" : "Radek"}
+# obiekt typu para klucz wartość
+# odpowiednikiem jsona w pythonie jest słownik
+
+person_dict = {'name': 'Radek', 'age': 40, 'czy_pali': None}
+print(type(person_dict))  # <class 'dict'>
+
+with open('nasze_dane.json', 'w') as f:
+    json.dump(person_dict, f)
+
+# {"name": "Radek", "age": 40, "czy_pali": null}
+
+# upiększony json
+with open('nasze_dane.json', 'w') as f:
+    json.dump(person_dict, f, indent=4)
+# {
+#     "name": "Radek",
+#     "age": 40,
+#     "czy_pali": null
+# }
+
+# upiększony, klucze posortowane alfabetycznie
+with open('nasze_dane.json', 'w') as f:
+    json.dump(person_dict, f, indent=4, sort_keys=True)
+# {
+#     "age": 40,
+#     "czy_pali": null,
+#     "name": "Radek"
+# }
+
+with open('nasze_dane.json', 'r') as fh:
+    data = json.load(fh)  # wczytywanie jsona jako słownik
+
+print(data)  # {'age': 40, 'czy_pali': None, 'name': 'Radek'}
+print(type(data))  # <class 'dict'>
+print(data['name'])  # Radek
+
+# zamiana słowinka na jsona (str)
+json_text = json.dumps(data)
+print(json_text)  # {"age": 40, "czy_pali": null, "name": "Radek"}
+print(type(json_text))  # <class 'str'>
+
+# zamiana jsona (str) na słownik
+dict_json = json.loads(json_text)
+print(dict_json)  # {'age': 40, 'czy_pali': None, 'name': 'Radek'}
+print(type(dict_json))  # <class 'dict'>
